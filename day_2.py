4641d9b3cd87f17e041395e013e7d7a6469f9664
hemanthkumarra <229x1a33c8@gprec.ac.in>
2025-12-09 22:49:18 +0530

    day 2 working on type conversion

---
+++ day_2.py
@@ -0,0 +1,16 @@
+# # name=input("What's your name?\n")
+# # length=len(name)
+# # print("lenght of your name: "+ str(length))
+# #
+# # print(f"lenght od the name : {length}")
+#
+# print(type(6/2))
+# print(type(6//2))
+
+print("Welcome to the tip calculator!")
+bill=int(input("what was the total bill?"))
+tip=int(input("how much tip would you like to give? 10 12 15\n"))
+t=bill*(1+(tip/100))
+n=int(input("how many people to split the bill?"))
+div=(t/n)
+print(f"total of {t} Each person should pay: ${div}")
